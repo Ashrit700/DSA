54a3fad1e1f9650cebaf4fb773c2e8ac8e1c2ff7
Ashrit Shukla <ashritshukla34@gmail.com>
2026-07-15 12:03:30 +0530

    Bakwass sawal

---
+++ Streak/Day_03_break.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // if(n==1){
+        //     return 1;
+        // }
+        // int odd=n*n;
+        // int even=n*(n+1);
+        // int remainder=even%odd;
+        // while(remainder!=0){
+        //     odd=even;
+        //     even =remainder;
+            
+        //     remainder=odd%even;
+            
+
+        // }
+        // return even;
+        return n;
+        
+    }
+}
